9962083bddf9e92acc9d25a90bf774df38454db2
Vinay Phadnis <21256352+vinayphadnis@users.noreply.github.com>
2019-07-10 23:20:39 +0530

    basic working

--- ownpl/writefile.py
+++ ownpl/writefile.py
@@ -4,10 +4,10 @@ import os
 
 
 def generatefile(name):
-    inputFile = "demofile.txt"
-    inputf = open(inputFile, "r")
-    data = inputf.read()
-    inputf.close()  
+    bpFile = "demofile.txt"
+    bpf = open(bpFile, "r")
+    data = bpf.read()
+    bpf.close()  
     filename = name + str(".py")
 
     outputf = open(filename, "a")
@@ -15,7 +15,7 @@ def generatefile(name):
     outputf.close()
 
     # Optional command to test the working of the file
-    os.system("python " + filename + " " + inputFile )
+    os.system("python " + filename + " " + bpFile )
 
 
 
